420df58aa3ae3206ab59e70e28a3c7f2951eacb1
ArturoCruz606 <ar7ur0321@gmail.com>
2021-10-20 18:12:26 -0300

    Primer commit

---
+++ softwareFactory.Core/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace softwareFactory.Core
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
